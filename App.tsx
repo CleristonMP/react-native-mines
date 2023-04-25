114607016c1b3250b97b8f2d6d9d2dde7ab78ac4
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2023-04-25 11:40:43 -0300

    MineField component

--- App.tsx
+++ App.tsx
@@ -1,50 +1,51 @@
 import React, {Component} from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import params from './src/params';
-import Field from './src/components/Field';
+import {createMinedBoard} from './src/game-logics';
+import MineField from './src/components/MineField';
 
 export default class App extends Component {
+  constructor(props: any) {
+    super(props);
+    this.state = this.createState();
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount();
+    const rows = params.getRowsAmount();
+    return Math.ceil(cols * rows * params.difficultLevel);
+  };
+
+  createState = () => {
+    const cols = params.getColumnsAmount();
+    const rows = params.getRowsAmount();
+    return {
+      board: createMinedBoard(rows, cols, this.minesAmount),
+    };
+  };
+
   render() {
     return (
-      <SafeAreaView style={styles.sectionContainer}>
+      <SafeAreaView style={styles.container}>
         <Text style={styles.sectionTitle}>Iniciando o Mines!</Text>
         <Text style={styles.sectionDescription}>
           Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}
         </Text>
-        <Field />
-        <Field opened />
-        <Field opened nearMines={1} />
-        <Field opened nearMines={2} />
-        <Field opened nearMines={3} />
-        <Field opened nearMines={6} />
-        <Field mined />
-        <Field mined opened />
-        <Field mined opened exploded />
-        <Field flagged />
-        <Field flagged opened />
+        <View style={styles.board}>
+          <MineField board={this.state.board} />
+        </View>
       </SafeAreaView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
+  container: {
     flex: 1,
-    marginTop: 32,
-    paddingHorizontal: 24,
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
+  },
+  board: {
     alignItems: 'center',
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+    backgroundColor: '#AAA',
   },
 });
